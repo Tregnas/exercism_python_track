92663d319070267b4ad11863f00e688c4937bbef
tregnas <tregnas@gmail.com>
2022-04-28 11:49:09 +0400

    Solution of Triangle task: https://exercism.org/tracks/python/exercises/triangle upd: func input, scalene improvements

--- triangle.py
+++ triangle.py
@@ -1,33 +1,34 @@
 """
 Determine if a triangle is equilateral, isosceles, or scalene.
+
+sides: (list[int, int, int]) given sizes of triangle
+return: (bool) is triangle, equaliteral, isosceles, scalene or degenerate
 """
 
 def is_triangle(sides):
-"""
-Check is inputed sizes makes triangle at all
-"""
+    """
+    Check is inputed sizes makes triangle at all
+    """
     if 0 not in sides and sides[0] + sides[1] >= sides[2] and sides[1] + sides[2] >= sides[0] and sides[0] + sides[2] >= sides[1]:
             return True
     return False
 
 def equilateral(sides):
-"""
-An equilateral triangle has all three sides the same length.
-"""
+    """
+    An equilateral triangle has all three sides the same length.
+    """
     if is_triangle(sides):
-        if sides[0] == sides[1]:
-            if sides[1] == sides[2]:
-                if sides[0] == sides[2]:
-                    return True
+        if sides[0] == sides[1] == sides[2]:
+                return True
     return False
 # alt oneliner: convert list into set, cos values in set are unique, so...
 #    return len(set(sides)) == 1
 
 def isosceles(sides):
-"""
-An isosceles triangle has at least two sides the same length. 
-(It is sometimes specified as having exactly two sides the same length, but we'll say at least two.)
-"""
+    """
+    An isosceles triangle has at least two sides the same length. 
+    (It is sometimes specified as having exactly two sides the same length, but we'll say at least two.)
+    """
     if is_triangle(sides):
         if sides[0] == sides[1]:
             return True
@@ -39,9 +40,9 @@ An isosceles triangle has at least two sides the same length.
 # alt oneliner: return len(set(sides)) < 3
 
 def scalene(sides):
-"""
-A scalene triangle has all sides of different lengths.
-"""
+    """
+    A scalene triangle has all sides of different lengths.
+    """
     if is_triangle(sides):
         if equilateral(sides) == isosceles(sides) == False:
             return True
@@ -49,11 +50,12 @@ A scalene triangle has all sides of different lengths.
 # alt oneliner for 2nd if: return len(set(sides)) == 3
 
 def degenerate(sides):
-"""
-The case where the sum of the lengths of two sides equals that of the third is known as a degenerate triangle.
-It has zero area and looks like a single line.
-"""
+    """
+    The case where the sum of the lengths of two sides equals that of the third is known as a degenerate triangle.
+    It has zero area and looks like a single line.
+    """
     if is_triangle(sides):
         if sides[0] + sides[1] == sides[2] or sides[1] + sides[2] == sides[0] or sides[0] + sides[2] == sides[1]:
             return True
     return False
+# alt oneliner for 2nd if: return len(set(sides)) == 3
